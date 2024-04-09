a141329d992961750483a1a70a5d6004ad91d687
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:19:44 +0300

    1

---
+++ Array.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_03
+{
+    using System;
+
+    using System;
+
+    public class IntArray : IOutput
+    {
+        private int[] elements;
+
+        public IntArray(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Array elements:");
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine($"Array elements ({info}):");
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+using _3_03;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        IntArray intArray = new IntArray(numbers);
+
+        intArray.Show();
+        intArray.Show("Test info");
+    }
+}
+
+
