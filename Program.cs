84c905dd9cfd91ce5d8614f4b8a5329ee63dfa74
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:35:03 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Collections;
+namespace _3_03; 
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = { 1, 2, 3, 4, 5 };
+            IntArray intArray = new IntArray(elements);
+
+            Console.WriteLine($"Кількість елементів менших за 3: {intArray.Less(3)}");
+            Console.WriteLine($"Кількість елементів більших за 3: {intArray.Greater(3)}");
+        }
+    }
+
